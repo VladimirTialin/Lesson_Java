46423b83a65aaff71d68d34e6dfde7e26dffd0ae
Vladimir <wel.tall@yandex.ru>
2022-08-30 21:18:49 +0300

    Первый запуск

---
+++ program.java
@@ -0,0 +1,14 @@
+public class program{
+    public static void main(String[] args) {
+        String s="world";
+        System.out.println(s.charAt(0));
+        float f=2.7f;// обязательно f в конце
+        System.out.println(f);
+        System.out.println(Integer.MAX_VALUE);
+        int i=10;
+        i=i-- - --i;
+        System.out.println(i);
+        //System.out.println(i++);
+        //System.out.println(++i);
+    }
+}
